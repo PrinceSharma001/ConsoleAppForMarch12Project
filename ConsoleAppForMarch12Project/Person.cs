16df5c13a6170887da1ffdffe99169ae48718a56
Prince Sharma <pshar179@my.centennialcollege.ca>
2020-04-14 09:55:57 -0400

    person

---
+++ ConsoleAppForMarch12Project/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppForMarch12Project
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+
+        public string ToFunnyString()
+        {
+            if(Age < 2)
+            {
+                return "Baby";
+            }
+            else if(Age < 3)
+            {
+                return "Terrible Two";
+            }
+            else
+        }
+    }
+}
